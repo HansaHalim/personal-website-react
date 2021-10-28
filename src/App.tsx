27674b75f5c152bcef9f535dff9f9b2525d5c2c3
Hansa Halim <hhalim@uwaterloo.ca>
2021-10-27 23:54:18 -0400

    Added basename to browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route path="/" exact component={() => <Home />} />
           <Route path="/blog" exact component={() => <Blog />} />
